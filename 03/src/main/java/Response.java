8853ade5529d0416a68ace5fc20e69d11c3879d8
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2021-09-09 17:25:04 +0200

    refactor

--- 03/src/main/java/Response.java
+++ 03/src/main/java/Response.java
@@ -4,12 +4,12 @@ public class Response {
     private static final String SPACES_MSG = "spacje";
     private static final String NUMBERS_MSG = "liczby";
     private static final String OTHERS_MSG = "inne";
-    public static final String NEW_LINE = "\n";
+    private static final String NEW_LINE = "\n";
 
     private final String value;
 
     public Response(int letters, int numbers, int spaces, int others) {
-        value = addLine(letters, LETTERS_MSG) +
+        this.value = addLine(letters, LETTERS_MSG) +
                 addLine(spaces, SPACES_MSG) +
                 addLine(numbers, NUMBERS_MSG) +
                 addLine(others, OTHERS_MSG);
